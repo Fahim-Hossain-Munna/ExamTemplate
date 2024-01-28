65b4488110805d61668564d399269f2445459801
fahim-hossain-munna <fahim.education.bd@gmail.com>
2024-01-28 14:54:49 +0600

    some bug in object try to resolve

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from "vue";
 
+let ans = ref('')
+
 const questions = [
     {
         id:1,
@@ -76,39 +78,29 @@ const questions = [
             <h2 class="text-2xl font-bold mb-4">Date : <span class="text-2xl font-semibold mb-4">{{ new Date() }}</span> </h2>
             
         </div>
-
-        <div class="max-w-md mx-auto bg-white rounded p-8 shadow-md">
+        {{ans}}
+        <div class="max-w-md mx-auto bg-white rounded px-8 py-3 shadow-md" v-for="(text,index) in questions" :key="text.id">
         <!-- Question Section -->
         <div class="mb-4">
-            <h2 class="text-lg font-semibold mb-2">Question 1:</h2>
-            <p class="text-gray-700">What is the capital of France?</p>
+            <h2 class="text-lg font-semibold mb-2">Question {{ ++index }}:</h2>
+            <p class="text-gray-700">{{ text.question }}?</p>
         </div>
 
         <!-- Options Section -->
-        <div class="mb-6">
+        <div class="mb-2">
             <label class="block text-sm font-medium text-gray-700">Options:</label>
             <div class="mt-2 flex flex-col">
-                <label class="">
-                    <input type="radio" class="form-radio text-indigo-600" name="question1" value="option1" v-model="ans">
-                    <span class="ml-2">Option A</span>
-                </label>
-                <label class="">
-                    <input type="radio" class="form-radio text-indigo-600" name="question2" value="option2" v-model="ans">
-                    <span class="ml-2">Option B</span>
-                </label>
-                <label class="">
-                    <input type="radio" class="form-radio text-indigo-600" name="question3" value="option3" v-model="ans">
-                    <span class="ml-2">Option C</span>
-                </label>
-                <label class="">
-                    <input type="radio" class="form-radio text-indigo-600" name="question4" value="option4" v-model="ans">
-                    <span class="ml-2">Option D</span>
+                <label class="" v-for="(select,index) in text.options" :key="index">
+                    <input type="radio" class="form-radio text-indigo-600" name="question1" :value=select v-model="ans">
+                    <span class="ml-2">{{ select }}</span>
                 </label>
             </div>
         </div>
 
-        <!-- Submit Button -->
-        <button class="bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-600">Submit Answer</button>
     </div>
+        <div class="max-w-md mx-auto bg-white rounded p-8 shadow-md">
+            <!-- Submit Button -->
+        <button class="bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-600">Submit Answer</button>
+        </div>
 </template>
 
