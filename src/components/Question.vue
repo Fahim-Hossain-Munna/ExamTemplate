07f1477e716e810d5feefcd9fbf217d9f807f279
fahim-hossain-munna <fahim.education.bd@gmail.com>
2024-01-28 14:35:42 +0600

    object id insert

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -3,51 +3,61 @@ import { ref } from "vue";
 
 const questions = [
     {
+        id:1,
         question: "What is the capital of France?",
         options: ["Paris", "Berlin", "Madrid", "Rome"],
         correctAnswer: "Paris"
     },
     {
+        id:2,
         question: "Which planet is known as the Red Planet?",
         options: ["Earth", "Mars", "Jupiter", "Venus"],
         correctAnswer: "Mars"
     },
     {
+        id:3,
         question: "In which year did World War II end?",
         options: ["1943", "1945", "1950", "1939"],
         correctAnswer: "1945"
     },
     {
+        id:4,
         question: "What is the largest mammal in the world?",
         options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
         correctAnswer: "Blue Whale"
     },
     {
+        id:5,
         question: "Who wrote 'Romeo and Juliet'?",
         options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "Leo Tolstoy"],
         correctAnswer: "William Shakespeare"
     },
     {
+        id:6,
         question: "What is the capital of Japan?",
         options: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
         correctAnswer: "Tokyo"
     },
     {
+        id:7,
         question: "Which element has the chemical symbol 'H'?",
         options: ["Helium", "Hydrogen", "Hassium", "Hafnium"],
         correctAnswer: "Hydrogen"
     },
     {
+        id:8,
         question: "Who painted the Mona Lisa?",
         options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"],
         correctAnswer: "Leonardo da Vinci"
     },
     {
+        id:9,
         question: "What is the currency of Brazil?",
         options: ["Euro", "Dollar", "Yen", "Real"],
         correctAnswer: "Real"
     },
     {
+        id:10,
         question: "What is the largest ocean on Earth?",
         options: ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"],
         correctAnswer: "Pacific Ocean"
